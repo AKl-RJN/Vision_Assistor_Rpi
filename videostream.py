a7ddee948e4e488db27f6c339d92e8e56aec45ee
AKl-RJN <70215888+AKl-RJN@users.noreply.github.com>
2021-08-24 15:50:21 -0400

    Update videostream.py

--- videostream.py
+++ videostream.py
@@ -4,30 +4,26 @@ import webcamvideostream
 class VideoStream:
 	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
 		framerate=32, **kwargs):
-		# check to see if the picamera module should be used
+		
 		if usePiCamera:
-			# only import the picamera packages unless we are
-			# explicity told to do so -- this helps remove the
-			# requirement of `picamera[array]` from desktops or
-			# laptops that still want to use the `imutils` package
+			
 			import pivideoStream
 
-			# initialize the picamera stream and allow the camera
-			# sensor to warmup
+			# initialize the picamera stream 
 			self.stream = PiVideoStream(resolution=resolution,
 				framerate=framerate, **kwargs)
 
-		# otherwise, we are using OpenCV so initialize the webcam
-		# stream
+		# or we are using OpenCV so initialize the webcam
+		
 		else:
 			self.stream = WebcamVideoStream(src=src)
 
 	def start(self):
-		# start the threaded video stream
+		
 		return self.stream.start()
 
 	def update(self):
-		# grab the next frame from the stream
+		
 		self.stream.update()
 
 	def read(self):
@@ -35,5 +31,5 @@ class VideoStream:
 		return self.stream.read()
 
 	def stop(self):
-		# stop the thread and release any resources
+		
 		self.stream.stop()
